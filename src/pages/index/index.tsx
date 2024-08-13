cb2c124bea025c504ce8ce2ba051df9a8a5262df
Tangjj1996 <awesometangji@gmail.com>
2024-08-13 16:06:04 +0800

    feat: 抽奖小程序

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,15 +1,41 @@
-import { View, Text } from '@tarojs/components';
+import { useRef } from 'react';
+import { View } from '@tarojs/components';
 import { useLoad } from '@tarojs/taro';
+import { LuckyWheel } from '@lucky-canvas/taro/react';
+import { options } from './shared';
 import './index.less';
 
 export default function Index() {
+  const lukyRef = useRef<any>();
+
   useLoad(() => {
     console.log('Page loaded.');
   });
 
   return (
-    <View className="index">
-      <Text>Hello world!</Text>
+    <View>
+      <LuckyWheel
+        width="300px"
+        height="300px"
+        {...options}
+        ref={lukyRef}
+        onStart={() => {
+          // 点击抽奖按钮会触发star回调
+          // 调用抽奖组件的play方法开始游戏
+          lukyRef.current?.play?.();
+          // 模拟调用接口异步抽奖
+          setTimeout(() => {
+            // 假设后端返回的中奖索引是0
+            const index = 0;
+            // 调用stop停止旋转并传递中奖索引
+            lukyRef.current?.stop?.(index);
+          }, 2500);
+        }}
+        onEnd={(prize) => {
+          // 抽奖结束会触发end回调
+          console.log(prize);
+        }}
+      />
     </View>
   );
 }
---
+++ src/pages/index/shared.ts
@@ -0,0 +1,21 @@
+export const options = {
+  blocks: [{ padding: '13px', background: '#617df2' }],
+  prizes: [
+    { fonts: [{ text: '0', top: '10%' }], background: '#e9e8fe' },
+    { fonts: [{ text: '1', top: '10%' }], background: '#b8c5f2' },
+    { fonts: [{ text: '2', top: '10%' }], background: '#e9e8fe' },
+    { fonts: [{ text: '3', top: '10%' }], background: '#b8c5f2' },
+    { fonts: [{ text: '4', top: '10%' }], background: '#e9e8fe' },
+    { fonts: [{ text: '5', top: '10%' }], background: '#b8c5f2' },
+  ],
+  buttons: [
+    { radius: '50px', background: '#617df2' },
+    { radius: '45px', background: '#afc8ff' },
+    {
+      radius: '40px',
+      background: '#869cfa',
+      pointer: true,
+      fonts: [{ text: '开始\n抽奖', top: '-20px' }],
+    },
+  ],
+};
