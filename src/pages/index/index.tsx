827def8a1e47d432b7a9b6f85e3d6783196a8af8
Tangjj1996 <awesometangji@gmail.com>
2024-08-13 22:09:52 +0800

    feat: 实时配置按钮 Loading 状态

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -2,11 +2,14 @@ import { useRef } from 'react';
 import { navigateTo } from '@tarojs/taro';
 import { View, Button } from '@tarojs/components';
 import { useRealTimeStore } from '@/stores/real-time-config';
+import { useBoolean } from 'ahooks';
 import { LuckyWheel } from '@lucky-canvas/taro/react';
 
 export default function Index() {
   const lukyRef = useRef<any>();
   const { blocks, buttons, prizes } = useRealTimeStore();
+  const [isWheeling, { setTrue: setWheelTrue, setFalse: setWheelFalse }] =
+    useBoolean(false);
 
   return (
     <View>
@@ -21,6 +24,7 @@ export default function Index() {
           onStart={() => {
             // 点击抽奖按钮会触发star回调
             // 调用抽奖组件的play方法开始游戏
+            setWheelTrue();
             lukyRef.current?.play?.();
             // 模拟调用接口异步抽奖
             setTimeout(() => {
@@ -30,11 +34,12 @@ export default function Index() {
           }}
           onEnd={(prize) => {
             // 抽奖结束会触发end回调
-            console.log(prize);
+            setWheelFalse();
           }}
         />
       </View>
       <Button
+        disabled={isWheeling}
         type="primary"
         className="mt-10 ml-5 mr-5"
         onClick={() => {
