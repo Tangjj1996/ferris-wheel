c2310bd30f33cc539c9f538d8f306a72e0b714e1
Tangjj1996 <awesometangji@gmail.com>
2024-08-24 18:52:10 +0800

    feat: 登录接口调一次就行

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,6 @@
 import { PropsWithChildren } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { useLaunch, login, setStorageSync } from '@tarojs/taro';
+import { useLaunch, login, setStorageSync, getStorageSync } from '@tarojs/taro';
 import { getOpenid } from './api/common/getOpenid';
 import { getConfig } from './api/common/getConfig';
 import './app.less';
@@ -11,12 +11,14 @@ const queryClient = new QueryClient();
 
 function App({ children }: PropsWithChildren<any>) {
   useLaunch(async () => {
-    const { code } = await login();
-    const { data } = (await getOpenid(code)) || {};
-    const {
-      data: { openid },
-    } = data || {};
-    setStorageSync(LocalStorageKey.openId, openid);
+    if (!getStorageSync(LocalStorageKey.openId)) {
+      const { code } = await login();
+      const { data } = (await getOpenid(code)) || {};
+      const {
+        data: { openid },
+      } = data || {};
+      setStorageSync(LocalStorageKey.openId, openid);
+    }
     await getConfig();
   });
 
