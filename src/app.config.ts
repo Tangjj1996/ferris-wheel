6cab665ead623e0c2b9c6975619ba77d1c0fc57e
Tangjj1996 <awesometangji@gmail.com>
2024-09-09 22:25:58 +0800

    feat: 增加图标

--- src/app.config.ts
+++ src/app.config.ts
@@ -17,18 +17,26 @@ export default defineAppConfig({
       {
         pagePath: 'pages/index/index',
         text: '大盘',
+        iconPath: './assets/icon/house.png',
+        selectedIconPath: './assets/icon/house_blue.png',
       },
       {
         pagePath: 'pages/search/index',
         text: '热门',
+        iconPath: './assets/icon/flame.png',
+        selectedIconPath: './assets/icon/flame_blue.png',
       },
       {
         pagePath: 'pages/config/index',
         text: '配置',
+        iconPath: './assets/icon/settings.png',
+        selectedIconPath: './assets/icon/settings_blue.png',
       },
       {
         pagePath: 'pages/mine/index',
         text: '我的',
+        iconPath: './assets/icon/user.png',
+        selectedIconPath: './assets/icon/user_blue.png',
       },
     ],
   },
